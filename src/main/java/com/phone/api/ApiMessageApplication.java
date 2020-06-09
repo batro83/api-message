688b024b6ed14ab2d2f81ba4797a6b25bb378472
batro83 <batro_ska@yahoo.es>
2020-06-09 09:19:59 +0200

    insert providers to mongodb when start app

--- src/main/java/com/phone/api/ApiMessageApplication.java
+++ src/main/java/com/phone/api/ApiMessageApplication.java
@@ -1,14 +1,35 @@
 package com.phone.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class})
-public class ApiMessageApplication {
+import com.phone.api.dao.ProviderDao;
+import com.phone.api.model.Provider;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class ApiMessageApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProviderDao providerDao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiMessageApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		providerDao.deleteAll();
+		Provider provider1 = Provider.builder().id("1").name("P1").prefix("0034").cost(1).build();
+		providerDao.save(provider1);
+		Provider provider2 = Provider.builder().id("2").name("P2").prefix("0034").cost(2).build();
+		providerDao.save(provider2);
+		Provider provider3 = Provider.builder().id("3").name("P3").prefix("0034").cost(1).build();
+		providerDao.save(provider3);
+		Provider provider4 = Provider.builder().id("4").name("P3").prefix("0033").cost(3).build();
+		providerDao.save(provider4);
 	}
 
 }
